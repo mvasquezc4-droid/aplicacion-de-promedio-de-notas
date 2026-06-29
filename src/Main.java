620fd7ae19cb805b13d55feaa0c0eadb0fe848b6
mvasquezc4-droid <mvasquezc4@unicartagena.edu.co>
2026-06-28 20:47:05 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
-import java.util.Scanner;class NotaDefinitiva {
+import java.util.Scanner;
+
+public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("=== CALCULADORA DE NOTA DEFINITIVA===");
-        System.out.println("Nombre del estudiante: ");
+
+        System.out.println("=== CALCULADORA DE NOTA DEFINITIVA ===");
+
+        System.out.print("Nombre del estudiante: ");
         String estudiante = sc.nextLine();
-        System.out.println("Nombre de la asignatura: ");
+
+        System.out.print("Nombre de la asignatura: ");
         String asignatura = sc.nextLine();
 
-        double nota1, nota2, nota3, nota4;
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+
         do {
-            System.out.print("Ingrese la Nota I (0-5): ");
+            System.out.print("Ingrese la Nota I (0 - 5): ");
             nota1 = sc.nextDouble();
         } while (nota1 < 0 || nota1 > 5);
 
         do {
-            System.out.print("Ingrese la Nota II (0-5):");
+            System.out.print("Ingrese la Nota II (0 - 5): ");
+            nota2 = sc.nextDouble();
         } while (nota2 < 0 || nota2 > 5);
 
         do {
-            System.out.print("Ingrese la Nota III (0-5)");
+            System.out.print("Ingrese la Nota III (0 - 5): ");
+            nota3 = sc.nextDouble();
         } while (nota3 < 0 || nota3 > 5);
 
         do {
-            System.out.print("Ingrese la Nota IV (0-5):");
+            System.out.print("Ingrese la Nota IV (0 - 5): ");
+            nota4 = sc.nextDouble();
         } while (nota4 < 0 || nota4 > 5);
+
         double corte1 = nota1 * 0.20;
         double corte2 = nota2 * 0.20;
         double corte3 = nota3 * 0.20;
@@ -31,9 +46,9 @@ import java.util.Scanner;class NotaDefinitiva {
 
         double definitiva = corte1 + corte2 + corte3 + corte4;
 
-        System.out.println("\n==== RESULTADOS ====");
-        System.out.println("Nombre del alumno: + estudiante");
-        System.out.println("Asignatura:" + asignatura);
+        System.out.println("\n===== RESULTADOS =====");
+        System.out.println("Nombre del alumno: " + estudiante);
+        System.out.println("Asignatura: " + asignatura);
 
         System.out.printf("Nota I: %.1f (20%%) = %.2f%n", nota1, corte1);
         System.out.printf("Nota II: %.1f (20%%) = %.2f%n", nota2, corte2);
