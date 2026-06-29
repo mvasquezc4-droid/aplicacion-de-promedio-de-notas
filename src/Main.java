9b6d7b1bc5b0652ec77c4c25e98883aeb5e84972
mvasquezc4-droid <mvasquezc4@unicartagena.edu.co>
2026-06-28 20:12:51 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;class NotaDefinitiva {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== CALCULADORA DE NOTA DEFINITIVA===");
+        System.out.println("Nombre del estudiante: ");
+        String estudiante = sc.nextLine();
+        System.out.println("Nombre de la asignatura: ");
+        String asignatura = sc.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double nota1, nota2, nota3, nota4;
+        do {
+            System.out.print("Ingrese la Nota I (0-5): ");
+            nota1 = sc.nextDouble();
+        } while (nota1 < 0 || nota1 > 5);
+
+        do {
+            System.out.print("Ingrese la Nota II (0-5):");
+        } while (nota2 < 0 || nota2 > 5);
+
+        do {
+            System.out.print("Ingrese la Nota III (0-5)");
+        } while (nota3 < 0 || nota3 > 5);
+
+        do {
+            System.out.print("Ingrese la Nota IV (0-5):");
+        } while (nota4 < 0 || nota4 > 5);
+        double corte1 = nota1 * 0.20;
+        double corte2 = nota2 * 0.20;
+        double corte3 = nota3 * 0.20;
+        double corte4 = nota4 * 0.40;
+
+        double definitiva = corte1 + corte2 + corte3 + corte4;
+
+        System.out.println("\n==== RESULTADOS ====");
+        System.out.println("Nombre del alumno: + estudiante");
+        System.out.println("Asignatura:" + asignatura);
+
+        System.out.printf("Nota I: %.1f (20%%) = %.2f%n", nota1, corte1);
+        System.out.printf("Nota II: %.1f (20%%) = %.2f%n", nota2, corte2);
+        System.out.printf("Nota III: %.1f (20%%) = %.2f%n", nota3, corte3);
+        System.out.printf("Nota IV: %.1f (40%%) = %.2f%n", nota4, corte4);
+
+        if (definitiva >= 3.0) {
+            System.out.printf("NOTA DEFINITIVA: %.2f (Aprobada)%n", definitiva);
+        } else {
+            System.out.printf("NOTA DEFINITIVA: %.2f (Reprobada)%n", definitiva);
         }
+
+        sc.close();
     }
 }
